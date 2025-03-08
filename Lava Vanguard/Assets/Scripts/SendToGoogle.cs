1d0575ef4fec3d85616be85f9b2e9f0f2763d901
yuchenq24 <yuchenq@usc.edu>
2025-03-07 16:39:18 -0800

    data_collection_health_and_exp

--- Lava Vanguard/Assets/Scripts/SendToGoogle.cs
+++ Lava Vanguard/Assets/Scripts/SendToGoogle.cs
@@ -13,7 +13,7 @@ public class SendToGoogle : MonoBehaviour
     public string startTime="";
     public string endTime="";
     public float maxRange=0;
-    public int finalHealth=0;
+    public float finalHealth=0;
 
     
     public void RecordEndTime()
@@ -33,18 +33,23 @@ public class SendToGoogle : MonoBehaviour
     
     public void Send()
     {
-        
+        PlayerView player = FindObjectOfType<PlayerView>();
+        expLevel = player.GetLevel();
+        finalHealth = player.GetCurrentHealth();
 
 
-        StartCoroutine(Post(sessionID.ToString(), startTime, endTime));
+
+        StartCoroutine(Post(sessionID.ToString(), startTime, endTime,expLevel.ToString(),finalHealth.ToString()));
     }
-    
-    private IEnumerator Post(string sessionID, string startTime, string endTime)
+
+    private IEnumerator Post(string sessionID, string startTime, string endTime, string expLevel, string finalHealth)
     {
         WWWForm form = new WWWForm();
         form.AddField("entry.1156616989", sessionID);
         form.AddField("entry.1095354465", startTime);
         form.AddField("entry.844017086", endTime);
+        form.AddField("entry.1140619881", expLevel);
+        form.AddField("entry.842781336", finalHealth);
 
         using (UnityWebRequest www = UnityWebRequest.Post("https://docs.google.com/forms/u/0/d/1tiA8a2FBAE7rsP9ABsVroo_ZxPtPlfyXlwR89PRpQC8/formResponse", form))
         {
